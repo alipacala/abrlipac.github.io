1120df31167da164e69a46fe644941aac06dd604
Abraham Lipa Calabilla <71676827+abrlipac@users.noreply.github.com>
2021-06-21 10:29:25 -0500

    Add files via upload

--- lab13-v2/calc.js
+++ lab13-v2/calc.js
@@ -1,37 +1,34 @@
 const expressionEl = document.getElementById('expression'),
   resultEl = document.getElementById('result'),
   table = document.getElementById('buttons')
-let expressionValue = ''
+let expressionValue = '',
+  result
 
 const appendExpressionEl = v => {
   expressionValue += v
   expressionEl.value = expressionValue
 }
 const clearScreen = () => {
-  expressionValue = expressionEl.innerText = resultEl.innerText = ''
+  expressionValue = expressionEl.value = resultEl.innerText = ''
 }
 
 table.addEventListener('click', e => {
   if (e.target.tagName === 'BUTTON') {
     buttonText = e.target.innerText
-    if (buttonText == 'x') {
-      appendExpressionEl('*')
-    } else if (buttonText == '÷') {
-      appendExpressionEl('/')
-    } else if (buttonText == 'mod') {
-      appendExpressionEl('%')
-    } else if (buttonText == 'C') {
-      clearScreen()
-    } else if (buttonText == '=') {
+    if (buttonText == 'x') appendExpressionEl('*')
+    else if (buttonText == '÷') appendExpressionEl('/')
+    else if (buttonText == 'mod') appendExpressionEl('%')
+    else if (buttonText == 'C') clearScreen()
+    else if (buttonText == '=') {
       try {
-        resultEl.innerText = eval(expressionValue)
+        result = eval(expressionValue)
       } catch (error) {
-        Android.showToast('¡Ingrese una expresión válida!')
+        Android.showToast('Ingrese una expresión válida')
         Android.incrErrorCount()
         clearScreen()
       }
-    } else {
-      appendExpressionEl(buttonText)
-    }
+      if (result == undefined) Android.showToast('Debe ingresar una expresión')
+      else resultEl.innerText = result
+    } else appendExpressionEl(buttonText)
   }
 })
